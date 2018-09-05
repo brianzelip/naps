28548b780099e3ec14626d169e2fdfb709eaa838
Brian Zelip <bzelip@gmail.com>
2018-09-05 11:01:21 -0400

    add Nap.js model

---
+++ models/Nap.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+// have to tell mongoose that we're using ES6 async/await
+mongoose.Promise = global.Promise;
+
+const napSchema = new mongoose.Schema(
+  {
+    date: {
+      type: Date,
+      required: 'A date must be entered!'
+    },
+    startTime: {
+      type: String,
+      required: 'A start time must be entered!'
+    },
+    endTime: {
+      type: String,
+      required: 'An end time must be entered!'
+    },
+    length: String,
+    notes: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+napSchema.pre('save', function(next) {
+  // TODO calculate the nap length
+  // this.length =
+  next();
+}); // needs to be a long-form function because we need `this`, so arrow func won't do
+
+// napSchema.pre('save', function(next) {
+//   if (!this.isModified('nameFirst') && !this.isModified('nameLast')) {
+//     next(); // skip it
+//     return; // stop this function from running
+//     // the above two lines could also be written as:
+//     // `return next();`.
+//     //
+//     // also, `isModified()` is a Mongoose method
+//   }
+//   this.nameFull = `${this.nameFirst} ${this.nameLast}`;
+//   // TODO make this more resilient so slugs are unique
+//   next();
+// }); // needs to be a long-form function because we need `this`, so arrow func won't do
+
+// napSchema.pre('save', function(next) {
+//   if (!this.isModified('nameFirst') && !this.isModified('nameLast')) {
+//     next(); // skip it
+//     return;
+//   }
+//   this.slug = slug(`${this.nameFirst} ${this.nameLast}`);
+//   // TODO make this more resilient so slugs are unique
+//   next();
+// }); // needs to be a long-form function because we need `this`, so arrow func won't do
+
+module.exports = mongoose.model('Nap', napSchema);
