6feaa189b8b8c80139398c90136c62ba10aeb54e
Brian Zelip <bzelip@gmail.com>
2018-09-11 09:51:59 -0400

    render nap duration in success flash message in controller

--- controllers/napController.js
+++ controllers/napController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Nap = mongoose.model('Nap');
+const duration = require('../helpers').duration;
 
 exports.newNap = (req, res) => {
   res.render('editNap', { title: 'add new nap' });
@@ -9,6 +10,12 @@ exports.newNapPOST = async (req, res) => {
   const nap = new Nap(req.body);
   await nap.save();
   // res.send(req.body);
-  req.flash('success', 'Member saved successfully!');
+  req.flash(
+    'success',
+    `The ${duration(
+      req.body.endTime,
+      req.body.startTime
+    )} minute nap saved successfully!`
+  );
   res.redirect('/');
 };
